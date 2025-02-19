b76f447a707d9e89380c6a3eea372809bdfd0903
Fatma Elzahraa Ashraf <fatmaserry16@gmail.com>
2025-02-19 10:17:09 +0200

    fix: removed all swiper imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/autoplay";
 import App from "./App";
 import { ThemeProvider } from "./context/ThemeContext";
 import { AppWrapper } from "./components/common/PageMeta";
